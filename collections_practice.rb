43584176c2c9414e62181ea6a595814197880123
Margot Konig <margot.konig@blackrock.com>
2019-05-17 15:29:44 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,64 @@
+def sort_array_asc(arr)
+  arr.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(arr)
+  arr.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(arr)
+  arr.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(arr)
+  arr[1], arr[2] = arr[2], arr[1]
+  arr
+end
+
+
+
+def reverse_array(arr)
+  arr.reverse do |value|
+end
+end
+
+def kesha_maker(arr)
+  new_arr = []
+  arr.each do |value|
+    value[2] = "$"
+  new_arr << value
+end
+new_arr
+end
+
+def find_a(arr)
+  arr.select do |string|
+    string.start_with?("a")
+end
+end
+
+def sum_array(arr)
+  arr.inject do |sum,number|
+    sum + number
+  end
+end
+
+def add_s(arr)
+  arr.each_with_index.collect do |word, index|
+    if index != 1 
+      word << "s"
+    else
+      word
+    end
+  end
+  end
+  
+  
+  
+  
